823fa465bf2e83abaaf6fe71c3b2d41ce77d5aea
javiddelossantos5 <javiddelossantos5@gmail.com>
2021-01-26 12:37:17 -0700

    multiple rounds complete

--- assets/js/game.js
+++ assets/js/game.js
@@ -9,56 +9,61 @@ var enemyHealth = 50;
 var enemyAttack = 12;
 
 var fight = function(enemyName) {
+    window.alert("Welcome to Robot Gladiators! Round " + ( i + 1 ) );
     // Alert players that they are starting the round
-    window.alert("Welcome to Robot Gladiators!");
-        while(enemyHealth > 0) {
-    var promptFight = window.prompt(`Would you like to FIGHT or SKIP this battle? Enter FIGHT or SKIP to choose.`);
-     //if player choses to fight, then fight
-     if(promptFight === "fight" ||  promptFight === "FIGHT") {
-        enemyHealth = enemyHealth - playerAttack;
-        console.log(`${playerName} attacked ${enemyName}. ${enemyName} now has ${enemyHealth} health remaining.`);
+    while(enemyHealth > 0 && playerHealth > 0) {
+        
+            var promptFight = window.prompt(`Would you like to FIGHT or SKIP this battle? Enter FIGHT or SKIP to choose.`);
 
-        if (enemyHealth <= 0) {
-            window.alert (`${enemyName} has died!`)
-        } else {
-            window.alert(`${enemyName} still has ${enemyHealth} health left`);
-        } 
-
-        playerHealth = playerHealth - enemyAttack  ;
-        console.log(`${enemyName} attacked ${playerName}. ${playerName} now has ${playerHealth} health remaining`);
-
-        if (playerHealth <= 0) {
-            window.alert(`${playerName} has died!`)
-        } else {
-            window.alert(`${playerName} still has ${playerHealth} health left.`)
-        }
-        } else if (promptFight === 'skip' || promptFight === 'SKIP') {
-        // If player choses to skip
+            if (promptFight === 'skip' || promptFight === 'SKIP') {
+            // If player choses to skip
             var confirmSkip = window.confirm(`Are you sure you'd like to quit?`);
             //if yes (true), leave fight
             if (confirmSkip) {
                 window.alert(`${playerName} has chosen to skip the fight. Goodbye!`);
                 //subtract money from playerMoney for the skipping
-                playerMoney = playerMoney - 2;
-            }
-            // if no (false), ask question again by running fight() again
-            else {
-                fight();
-            }
-        } else {
-            window.alert(`You need to choose a valid option. Try again!`)
+                playerMoney = playerMoney - 10;
+                console.log(`${playerMoney} playerMoney`);
+                break;
+            } 
         }
-    }   
-}
     
+        //remove enemy's health by subtracting the amount set in the playerAttack variable
+        enemyHealth = enemyHealth -playerAttack;
+        console.log(`${playerName} attacked ${enemyName}. ${enemyName} now has ${enemyHealth} health remaining`);
+         
+        //check enemy's health
+        if (enemyHealth <= 0) {
+            window.alert (`${enemyName} has died!`);
+
+
+            //award player money for winning
+            playerMoney = playerMoney + 20;
+
+            //leave while() loop since enemy is dead
+            break;
+    
+         } else {
+            window.alert(`${enemyName} still has ${enemyHealth} health left`);
+        } 
+
+        //remove player's health by subtracting the amount set in the enemyAttack variable
+        playerHealth = playerHealth - enemyAttack;
+        console.log(`${enemyName} attacked ${playerName}. ${playerName} now has ${playerHealth} health remaining`);
+
+        //check player's health
+        if(playerHealth <= 0) {
+            window.alert(`${playerName} has died!`);
+            //leave while() loop since player is dead
+            break;
+        } else {
+            window.alert(`${playerName} still has ${playerHealth} health left`);
+        }
+    }
+};
+
 for(var i = 0; i < enemyNames.length; i++) {
     var pickedEnemyName = enemyNames[i];
     enemyHealth = 50;
     fight(pickedEnemyName);
 }
-
-// Game States
-// "WIN" - Player robot has defeated all enemy-robots
-//    * Fight all enemy-robots
-//    * Defeat each enemy-robot
-// "LOSE" - Player robot's health is zero or less
